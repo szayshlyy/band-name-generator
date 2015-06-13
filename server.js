6f400d78bb80e987b725c48909a2d3373287ed7c
Sanita <szayshlyy@gmail.com>
2015-06-13 13:47:04 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var port = process.env.PORT || 3000;
+
+var Adjective = require('./lib/adjective.js');
+var Verb = require('./lib/verb.js');
+var Noun = require('./lib/noun.js');
+var getRandomWord = require('./lib/getRandomWord.js');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/app/'));
+
+var adjective = new Adjective();
+var verb = new Verb();
+var noun = new Noun();
+
+function postWord (word, wordObject) {
+  if (wordObject.hasOwnProperty(word)) {
+    return {msg: 'We already have your awesome word, ' + word + ', in our list.'};
+  }
+
+  wordObject[word] = true;
+  console.dir(wordObject);
+  return {msg: 'Thanks for submitting ' + word + '!'};
+};
+
+app.get('/adjective', function(req, res) {
+  res.json(getRandomWord(adjective));
+});
+
+app.get('/verb', function(req, res) {
+  res.json(getRandomWord(verb));
+});
+
+app.get('/noun', function(req, res) {
+  res.json(getRandomWord(noun));
+});
+
+app.post('/adjective', function(req, res) {
+  var word = postWord(req.body.word, adjective);
+  res.json(word);
+});
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
+
+app.listen(port, function() {
+  console.log('server starting. available at http://localhost:' + port);
+});
+
+// app.get('/', function(req, res) {
+//   res.send('hello, universe');
+// });
