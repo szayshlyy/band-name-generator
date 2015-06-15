137189b25fe2a7b0dcfb2bd3bbec61ad068c748b
Sanita <szayshlyy@gmail.com>
2015-06-14 23:34:36 -0700

    Committing final changes

--- app/script.js
+++ app/script.js
@@ -1,26 +1,27 @@
 'use strict';
 
+/// This will display band name on click.
 $(function() {
-
-  $("button").click(function() {
+  $("button").on("click",function() {
     $.get('adjective', function(response) {
       var adjective = response.word;
       $("#adjective").text(adjective);
     });
 
-    $.get('verb', function(response) {
+    $.get("verb", function(response) {
       var verb = response.word;
       $("#verb").text(verb);
     });
 
-    $.get('noun', function(response) {
+    $.get("noun", function(response) {
       var noun = response.word;
       $("#noun").text(noun);
     });
 
   });
 
-  $("#submitWords").on("submit", function(e) {
+/// This will take words that users submit.
+  $("#submitAdj").on("submit", function(e) {
     e.preventDefault();
 
     var adjective = $("input[name=adjective]").val();
@@ -33,7 +34,37 @@ $(function() {
         $("#adjectiveRes").text(adjectiveRes);
       });
     }
+  });
 
+  $("#submitVerb").on("submit", function(e) {
+    e.preventDefault();
+
+    var verb = $("input[name=verb]").val();
+    var verbPost;
+
+    if (verb) {
+      verbPost = {word: verb};
+      $.post("verb", verbPost, function(response) {
+        var verbRes = response.msg;
+        $("#verbRes").text(verbRes);
+      });
+    $("#submitVerb").val("");
+    }
+  });
+
+  $("#submitNoun").on("submit", function(e) {
+    e.preventDefault();
+
+    var noun = $("input[name=noun]").val();
+    var nounPost;
+
+    if (noun) {
+      nounPost = {word: noun};
+      $.post("noun", nounPost, function(response) {
+        var nounRes = response.msg;
+        $("#nounRes").text(nounRes);
+      });
+    }
   });
 
 });
